1e2982f658038b0f61b3bd7500a8f0dc0ec5445d
gzsewell <gzsewell@gmail.com>
2024-12-11 14:46:20 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Got a problem, yo i'll solve it.
+alert("You have been chosen to crack the combonation")
+
+//Truth Table 
+//if x === x True
+// x || y
+// x && y
+// x ! y
+const combo1 = 5 + 5;
+const combo2 = 2 * 20;
+const combo3 = (2 % 2) + 38;
+
+alert(`${combo1}-${combo2}-${combo3}`)
+
+alert("Hurry and crack the code!!")
+
